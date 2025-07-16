777d911094d360332e8f3d47943f07415f7fdc2b
Eloi DEMOLIS <eloi.demoli@gmail.com>
2025-07-16 08:57:53 +0200

    Add linked buffer and text render
    Signed-off-by: Eloi DEMOLIS <eloi.demoli@gmail.com>

--- main.rs
+++ main.rs
@@ -1,9 +1,13 @@
 #![no_main]
 #![no_std]
 
+use core::mem::size_of;
+
 //======================================
 // Standard constants
 //======================================
+const PAGE_SIZE: usize = 4*1024;
+
 // file descriptors
 const STDIN: u32 = 0;
 const STDOUT: u32 = 1;
@@ -12,6 +16,16 @@ const STDERR: u32 = 2;
 // ioctl
 const TCGETS: u64 = 0x5401;
 const TCSETS: u64 = 0x5402;
+
+// mmap
+const MAP_FILE: u32 = 0x00;
+const MAP_SHARED: u32 = 0x01;
+const MAP_PRIVATE: u32 = 0x02;
+const MAP_ANONYMOUS: u32 = 0x20;
+const PROT_NONE: u32 = 0x00;
+const PROT_READ: u32 = 0x01;
+const PROT_WRITE: u32 = 0x02;
+const PROT_EXEC: u32 = 0x04;
 
 //======================================
 // Minimal runtime
@@ -66,13 +80,37 @@ struct Vec<const N: usize, T> {
 //======================================
 // Standard macros
 //======================================
+#[macro_export]
+macro_rules! const_assert {
+    (@size $T:ident == $v:expr) => {
+        $crate::const_assert!(@eq ::core::mem::size_of::<$T>(), $v);
+    };
+    (@align $T:ident == $v:expr) => {
+        $crate::const_assert!(@eq ::core::mem::align_of::<$T>(), $v);
+    };
+    (@offset $T:ty[$field:ident] == $v:expr) => {
+        $crate::const_assert!(@eq ::core::mem::offset_of!($T, $field), $v);
+    };
+    (@eq $actual:expr, $expected:expr) => {
+        const _: [(); $expected as usize] = [(); $actual as usize];
+    };
+    ($cond:expr) => {
+        const _: () = if !$cond {
+            panic!()
+        };
+    };
+}
+
+#[macro_export]
 macro_rules! syscall {
-    (read,  $($t:tt)*) => { syscall!( 0 => i64, $($t)*) };
-    (write, $($t:tt)*) => { syscall!( 1 => i64, $($t)*) };
-    (mmap,  $($t:tt)*) => { syscall!( 9 => *mut u8, $($t)*) };
-    (ioctl, $($t:tt)*) => { syscall!(16 => u64, $($t)*) };
-    (exit,  $($t:tt)*) => { syscall!(60 => !, $($t)*) };
-    (60 => !, $rdi:expr $(,[$($args:tt)+])?) => {
+    (read   $($t:tt)*) => { syscall!( 0 => i64 $($t)*) };
+    (write  $($t:tt)*) => { syscall!( 1 => i64 $($t)*) };
+    (mmap   $($t:tt)*) => { syscall!( 9 => *mut u8 $($t)*) };
+    (ioctl  $($t:tt)*) => { syscall!(16 => u64 $($t)*) };
+    (fork   $($t:tt)*) => { syscall!(57 => u64 $($t)*) };
+    (execve $($t:tt)*) => { syscall!(59 => ! $($t)*) };
+    (exit   $($t:tt)*) => { syscall!(60 => ! $($t)*) };
+    /*(60 => !, $rdi:expr $(,[$($args:tt)+])?) => {
         core::arch::asm!(
             "mov rax, 60",
             "syscall",
@@ -80,6 +118,20 @@ macro_rules! syscall {
             options(noreturn)
             $($($args)+)?
         )
+    };*/
+    ($id:literal => ! $(,$rdi:expr $(,$rsi:expr $(,$rdx:expr $(,$r10:expr $(,$r8:expr $(,$r9:expr)?)?)?)?)?)? $(,[$($args:tt)+])?) => {
+        core::arch::asm!(
+            concat!("mov rax, ", $id),
+            "syscall",
+            $(in("rdi") $rdi,
+            $(in("rsi") $rsi,
+            $(in("rdx") $rdx,
+            $(in("r10") $r10,
+            $(in("r8")  $r8,
+            $(in("r9")  $r9,)?)?)?)?)?)?
+            options(noreturn)
+            $($($args)+)?
+        );
     };
     ($id:literal => $ret:ty $(,$rdi:expr $(,$rsi:expr $(,$rdx:expr $(,$r10:expr $(,$r8:expr $(,$r9:expr)?)?)?)?)?)? $(,[$($args:tt)+])?) => {{
         let ret: $ret;
@@ -99,22 +151,27 @@ macro_rules! syscall {
         ret
     }};
 }
+
+#[macro_export]
 macro_rules! print {
     ($($args:expr),* $(,)?) => {
         _printf(&[$(&$args,)*])
     };
 }
+#[macro_export]
 macro_rules! println {
     ($($args:expr),* $(,)?) => {
         print!($($args,)* "\n")
     };
 }
+#[macro_export]
 macro_rules! panic {
     ($($args:expr),* $(,)?) => {
         println!("panic at ", file!(), ":", line!(), "\n", $($args)*);
         unsafe { syscall!(exit, 1) }
     }
 }
+#[macro_export]
 macro_rules! assert {
     ($e:expr) => {
         if !$e {
@@ -122,6 +179,7 @@ macro_rules! assert {
         }
     }
 }
+#[macro_export]
 macro_rules! impl_display {
     (for $T:ty as slice) => {
         impl Display for $T {
@@ -254,6 +312,23 @@ impl<const N: usize, T> Vec<N, T> {
         unsafe { *self.arr.get_unchecked_mut(self.len) = e };
         self.len += 1;
     }
+    fn insert(&mut self, i: usize, e: T) {
+        if i < self.len {
+            unsafe { core::ptr::copy(
+                self.as_ptr().add(i),
+                self.as_mut_ptr().add(i+1),
+                self.len - i) };
+        }
+        unsafe { *self.arr.get_unchecked_mut(i) = e };
+        self.len += 1;
+    }
+    fn remove(&mut self, i: usize) {
+        unsafe { core::ptr::copy(
+            self.as_ptr().add(i+1),
+            self.as_mut_ptr().add(i),
+            self.len - i) };
+        self.len -= 1;
+    }
 }
 
 impl<const N: usize, T> core::ops::Deref for Vec<N, T> {
@@ -301,7 +376,72 @@ const OPOST: u32 = 0x00000001;
 const VMIN: usize = 6;
 const VTIME: usize = 5;
 
-#[derive(Clone)]
+//struct BigLink<const N: usize, T> {
+//    next: *const BigLink<N, T>,
+//    data: [T; N],
+//}
+//struct PageLink {
+//    next: *const PageLink,
+//    data: [u8; PAGE_SIZE - size_of::<*const PageLink>()],
+//}
+//const_assert!(@size PageLink == PAGE_SIZE);
+
+#[repr(C)]
+struct BigLink<const N: usize, Meta> {
+    next: *mut BigLink<N, Meta>,
+    meta: Meta,
+    //_: core::mem::PhantomData<N>,
+}
+
+impl<const N: usize, Meta> BigLink<N, Meta> {
+    const DATA_SIZE: usize = N*PAGE_SIZE - size_of::<Self>();
+    fn new(meta: Meta) -> *mut Self {
+        unsafe {
+            let ret = syscall!(mmap, 0, N * PAGE_SIZE, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0) as *mut Self;
+            (*ret).next = 0 as _;
+            (*ret).meta = meta;
+            ret
+        }
+    }
+    fn as_ref(&self) -> &[u8] {
+        unsafe {
+            let ptr = (self as *const Self).add(1) as *const u8;
+            core::slice::from_raw_parts(ptr, Self::DATA_SIZE)
+        }
+    }
+    fn as_mut(&self) -> &mut[u8] {
+        unsafe {
+            let ptr = (self as *const Self as *mut Self).add(1) as *mut u8;
+            core::slice::from_raw_parts_mut(ptr, Self::DATA_SIZE)
+        }
+    }
+}
+
+impl<const N: usize> BigLink<N, TermMeta> {
+    fn push(&mut self, b: u8) {
+        if self.meta.len < Self::DATA_SIZE {
+            self.as_mut()[self.meta.len] = b;
+            self.meta.len += 1;
+        } else {
+            panic!("Link full");
+        }
+    }
+    fn data(&self) -> &[u8] {
+        if self.meta.len < Self::DATA_SIZE {
+            &self.as_ref()[..self.meta.len]
+        } else {
+            panic!("Link full");
+        }
+    }
+}
+
+#[derive(Default)]
+struct TermMeta {
+    len: usize,
+    lines: usize,
+    cut: usize,
+}
+
 struct Panel {
     x: usize,
     y: usize,
@@ -309,6 +449,7 @@ struct Panel {
     h: usize,
     cx: usize,
     cy: usize,
+    buffer: *mut BigLink<1, TermMeta>,
 }
 
 struct Multiplexer {
@@ -325,7 +466,19 @@ impl Panel {
             h,
             cx: 0,
             cy: 0,
+            buffer: BigLink::new(TermMeta::default()),
         }
+    }
+    fn prompt(&mut self) {
+        //const PROMPT: &[u8] = b">>> ";
+        const PROMPT: &[u8] = b"";
+        unsafe {
+            for c in PROMPT {
+                (*self.buffer).push(*c);
+            }
+        }
+        self.cx = PROMPT.len();
+        print!("\x1b[", self.x+1, "G", PROMPT);
     }
     fn render(&self, index: usize, active: bool) {
         let borders = if active {
@@ -362,8 +515,20 @@ impl Multiplexer {
     }
 
     fn run(&mut self) {
+        let mut skip_render = false;
+        let mut prompt = true;
         loop {
-            self.render();
+            if !skip_render {
+                self.render();
+            }
+            let active = unsafe { self.panels.get_unchecked_mut(self.active) };
+            if prompt {
+                active.prompt();
+            }
+            skip_render = false;
+            prompt = false;
+            let w = active.w;
+            let m = unsafe { &mut (*active.buffer).meta };
             let mut x = 0u8;
             unsafe { syscall!(read, STDIN, &mut x, 1) };
             match x {
@@ -387,25 +552,30 @@ impl Multiplexer {
                             _print(&[x]);
                             _print(")");
                         }
-                        b'v' => self.split(true),
-                        b'h' => self.split(false),
+                        b'v' => if self.split(true) {
+                            prompt = true;
+                        }
+                        b'h' => if self.split(false) {
+                            prompt = true;
+                        }
                         b'n' => self.active = if self.active+1 == self.panels.len { 0 } else { self.active+1 },
                         b'p' => self.active = if self.active == 0 { self.panels.len-1 } else { self.active-1 },
                         b'd' => {
                             if self.panels.len == 1 {
                                 break;
                             }
-                            for i in self.active..self.panels.len()-1 {
-                                if i+1 >= self.panels.len {
-                                    break;
-                                }
-                                self.panels[i] = self.panels[i+1].clone();
-                            }
-                            self.panels.len -= 1;
+                            self.panels.remove(self.active);
+                            //for i in self.active..self.panels.len()-1 {
+                            //    if i+1 >= self.panels.len {
+                            //        break;
+                            //    }
+                            //    self.panels[i] = self.panels[i+1];
+                            //}
                             if self.active == self.panels.len {
                                 self.active = self.panels.len-1;
                             }
-                            _print("\x1b[2J")
+                            _print("\x1b[2J");
+                            prompt = true;
                         },
                         c => {
                             _print("ALT(");
@@ -414,30 +584,75 @@ impl Multiplexer {
                         }
                     }
                 }
-                c => _print(&[c]),
+                127 => unsafe {
+                    skip_render = true;
+                    if (*active.buffer).meta.len == 0 {
+                        continue;
+                    }
+                    (*active.buffer).meta.len -= 1;
+                    if active.cx == 0 {
+                        active.cx = active.w-1;
+                        if active.cy > 0 {
+                            active.cy -= 1;
+                            _print("\x1b[1A");
+                        }
+                    } else {
+                        active.cx -= 1;
+                    }
+                    print!("\x1b[", active.x+1+active.cx, "G");
+                }
+                b'\n' | b'\r' => unsafe {
+                    skip_render = true;
+                    //if syscall!(fork) == 0 {
+                    //    syscall!(execve, (*active.buffer).data().as_ptr(), 0, 0);
+                    //}
+                    (*active.buffer).push(b'\n');
+                    if active.cy < active.h-1 {
+                        active.cy += 1;
+                        _print("\x1b[1B");
+                    }
+                    prompt = true;
+                }
+                c => unsafe {
+                    skip_render = true;
+                    (*active.buffer).push(c);
+                    _print(&[c]);
+                    active.cx += 1;
+                    if active.cx >= active.w {
+                        active.cx = 0;
+                        if active.cy < active.h-1 {
+                            active.cy +=1;
+                            _print("\x1b[1B");
+                        }
+                        print!("\x1b[", active.x+1, "G");
+                    }
+                }
             }
         }
     }
 
-    fn split(&mut self, v: bool) {
+    fn split(&mut self, v: bool) -> bool {
         let active = unsafe { self.panels.get_unchecked_mut(self.active) };
-        let new = if v {
-            if active.w < 8 {
-                return;
+        let mut new = if v {
+            if active.w < 5*2+2 {
+                return false;
             }
             let w = active.w-2;
             active.w = w - w/2;
             Panel::new(active.x + active.w+2, active.y, w/2, active.h)
         } else {
-            if active.h < 4 {
-                return;
+            if active.h < 1*2+2 {
+                return false;
             }
             let h = active.h-2;
             active.h = h - h/2;
             Panel::new(active.x, active.y + active.h+2, active.w, h/2)
         };
-        self.active = self.panels.len;
-        self.panels.push(new);
+        //self.active = self.panels.len;
+        //self.panels.push(new);
+        self.active += 1;
+        self.panels.insert(self.active, new);
+        true
     }
 
     fn render(&self) {
